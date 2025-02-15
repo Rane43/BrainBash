9c2b15b21476373588998769ce4bdd6cbf60918f
Aaron Guirke <a00325354@student.tus.ie>
2025-02-15 13:50:14 +0000

    Initial commit

---
+++ src/main/java/com/tus/prometheus/BrainBash/BrainBashApplication.java
@@ -0,0 +1,13 @@
+package com.tus.prometheus.BrainBash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainBashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainBashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tus/prometheus/BrainBash/BrainBashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tus.prometheus.BrainBash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainBashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
