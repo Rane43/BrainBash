24004511f23d63db06ef5744765d64f9e83d1782
Aaron Guirke <a00325354@student.tus.ie>
2025-03-15 11:21:01 +0000

    Updated search by filters functionality

--- src/main/resources/static/quizzer/features/dashboard/dashboard.js
+++ src/main/resources/static/quizzer/features/dashboard/dashboard.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-	fetchQuizzes();
-	fetchAgeRatings();
-	fetchDifficultyRatings();
+	Promise.all([fetchAgeRatings(), fetchDifficultyRatings()]).then(displayQuizzesBySearch);
 	
 	$("#search-bar").on("keyup", function () {
 	    displayQuizzesBySearch();
@@ -16,17 +14,11 @@ $(document).ready(function () {
 	})
 });
 
-
-
 function displayQuizzesBySearch() {
 	let searchText = $("#search-bar").val().trim();
 	let ageRating = $("#age-rating-dropdown").val();
 	let difficultyRating = $("#difficulty-rating-dropdown").val();
-	
-	fetchQuizzesBySearch(searchText, difficultyRating, ageRating);	
-}
 
-function fetchQuizzesBySearch(searchText, difficultyRating, ageRating) {
 	$.ajax({
 		url: `/api/quizzes/search?middleTitle=${searchText}&difficultyRating=${difficultyRating}&ageRating=${ageRating}`,
 		method: "GET",
@@ -44,29 +36,7 @@ function fetchQuizzesBySearch(searchText, difficultyRating, ageRating) {
 }
 
 // Fetch Queries options
-/* AGE RATING */
-function fetchAgeRatings() {
-	$.ajax({
-		url: "/api/age-ratings",
-		method: "GET",
-		dataType: "json",
-		success: (ageRatings) => {
-			// Populate Categories
-			let ageRatingDropdown = document.getElementById("age-rating-dropdown");
-			ageRatingDropdown.innerHTML = "";
-			
-			// Default 'All Age Ratings'
-			addOption(ageRatingDropdown, "", "All Age Ratings");
-			ageRatings.forEach((ageRating) => {
-				addOption(ageRatingDropdown, ageRating, ageRating);
-			});
-		}, 
-		error: () => {
-			console.log("Error retrieving categories");
-		}
-	});
-}
-
+/* AGE RATINGS */
 function addOption(dropdown, value, text) {
 	let option = document.createElement("option");
 	option.setAttribute("value", value);
@@ -74,51 +44,64 @@ function addOption(dropdown, value, text) {
 	dropdown.add(option);
 }
 
+function fetchAgeRatings() {
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: "/api/age-ratings",
+			method: "GET",
+			dataType: "json",
+			success: (ageRatings) => {
+				// Populate Age Ratings
+				let ageRatingDropdown = document.getElementById("age-rating-dropdown");
+				ageRatingDropdown.innerHTML = "";
+				
+				// Default 'All Age Ratings'
+				addOption(ageRatingDropdown, "", "All Age Ratings");
+				ageRatings.forEach((ageRating) => {
+					addOption(ageRatingDropdown, ageRating, ageRating);
+				});
+				
+				resolve();
+			}, 
+			error: () => {
+				console.log("Error retrieving categories");
+				reject();
+			}
+		});
+	})
+}
 
-/* DIFFICULTY RATING */
+/* DIFFICULTY RATINGS */
 function fetchDifficultyRatings() {
-	$.ajax({
-		url: "/api/difficulty-ratings",
-		method: "GET",
-		dataType: "json",
-		success: (difficultyRatings) => {
-			// Populate Categories
-			let selectDropdown = document.getElementById("difficulty-rating-dropdown");
-			selectDropdown.innerHTML = "";
-			
-			difficultyRatings.forEach((difficultyRating) => {
-				let option = document.createElement("option");
-				option.setAttribute("value", difficultyRating);
-				option.text=difficultyRating;
-				selectDropdown.add(option);
-			});
-		}, 
-		error: () => {
-			console.log("Error retrieving categories");
-		}
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: "/api/difficulty-ratings",
+			method: "GET",
+			dataType: "json",
+			success: (difficultyRatings) => {						
+				// Populate Difficulty Ratings
+				let difficultyRatingDropdown = document.getElementById("difficulty-rating-dropdown");
+				difficultyRatingDropdown.innerHTML = "";
+				
+				// Default 'All Difficulty Ratings'
+				addOption(difficultyRatingDropdown, "", "All Difficulty Ratings");
+				difficultyRatings.forEach((difficultyRating) => {
+					addOption(difficultyRatingDropdown, difficultyRating, difficultyRating);
+				});
+				
+				resolve();
+			}, 
+			error: () => {
+				console.log("Error retrieving categories");
+				reject();
+			}
+		});
 	});
 }
 
 
 
 // --------- Fetch queries -----------
-function fetchQuizzes() {
-	$.ajax({
-		url: "/api/quizzes",
-		method: "GET",
-		headers: {
-			"Authorization": `Bearer ${TokenStorage.getToken()}`
-		},
-		dataType: "json",
-		success: (quizSummaryDtos) => {
-			displayQuizzes(quizSummaryDtos);
-		},
-		error: () => {
-			console.log("Error retrieving quizzes...");
-		}
-	});
-}
-
 function displayQuizzes(quizSummaryDtos) {
 	// Empty previous content
 	$("#quiz-display").empty();
