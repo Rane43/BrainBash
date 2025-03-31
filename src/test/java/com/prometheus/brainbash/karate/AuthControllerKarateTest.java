f8285369018bc2b191055d2b03f33d0b3d0e6397
Aaron Guirke <a00325354@student.tus.ie>
2025-03-31 08:28:43 +0100

    Updated tests to run from a fresh start

--- src/test/java/com/prometheus/brainbash/karate/AuthControllerKarateTest.java
+++ src/test/java/com/prometheus/brainbash/karate/AuthControllerKarateTest.java
@@ -1,15 +1,30 @@
 package com.prometheus.brainbash.karate;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.intuit.karate.junit5.Karate;
+import com.prometheus.brainbash.test_helper.DatabaseManager;
 
 /*
  * Karate tests for AuthController
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestInstance(Lifecycle.PER_CLASS)
 public class AuthControllerKarateTest {
 	private static final String FOLDER = "classpath:features/karate/auth_controller/";
+	
+	@Autowired
+	private DatabaseManager databaseManager;
+	
+	@BeforeAll
+	void setupAll() {
+		databaseManager.executeUserSetupScript();
+	}
 	
 	@Karate.Test
 	Karate runLoginTests() {
@@ -17,5 +32,10 @@ public class AuthControllerKarateTest {
 				FOLDER + "login.feature",
 				FOLDER + "register.feature"
 		).relativeTo(getClass());
+	}
+	
+	@AfterAll
+	void teardownAll() {
+		databaseManager.clearDatabase();
 	}
 }
--- src/test/java/com/prometheus/brainbash/karate/DifficultyRatingControllerKarateTest.java
+++ src/test/java/com/prometheus/brainbash/karate/DifficultyRatingControllerKarateTest.java
@@ -1,15 +1,35 @@
 package com.prometheus.brainbash.karate;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.intuit.karate.junit5.Karate;
+import com.prometheus.brainbash.test_helper.DatabaseManager;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestInstance(Lifecycle.PER_CLASS)
 class DifficultyRatingControllerKarateTest {
 	private static final String FOLDER = "classpath:features/karate/difficulty_rating_controller/";
+	
+	@Autowired
+	private DatabaseManager databaseManager;
+	
+	@BeforeAll
+	void setupAll() {
+		databaseManager.executeUserSetupScript();
+	}
 	
 	@Karate.Test
 	Karate funAllDifficultyRatingKarateTests() {
 		return Karate.run(FOLDER + "get_all_difficulty_ratings.feature");
+	}
+
+	@AfterAll
+	void teardownAll() {
+		databaseManager.clearDatabase();
 	}
 }
