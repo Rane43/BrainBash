88237357dc654f9bd68510f1c21889f3a64ab195
A00325354 <A00325354@host.docker.internal>
2025-04-01 15:43:43 +0100

    Added option to help selenium test pass on jenkins running in docker

--- src/test/java/com/prometheus/brainbash/selenium/LoginSeleniumTest.java
+++ src/test/java/com/prometheus/brainbash/selenium/LoginSeleniumTest.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,7 +51,10 @@ class LoginSeleniumTest {
 		databaseManager.executeSetupScripts();
 		
 		WebDriverManager.chromedriver().setup();
-	    driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        
+	    driver = new ChromeDriver(options);
 	    wait = new WebDriverWait(driver, Duration.ofSeconds(20)); // 20 second timeout
 	}
 	
