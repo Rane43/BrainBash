b3eca7e4d6be54f68c44a99e980879fc5f8c2136
A00325354 <A00325354@host.docker.internal>
2025-04-03 19:53:57 +0100

    Test for Continuous build assignment

--- src/main/java/com/prometheus/brainbash/BrainBashApplication.java
+++ src/main/java/com/prometheus/brainbash/BrainBashApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BrainBashApplication {
-	// NGrok test
+	// Test
 	public static void main(String[] args) {
 		SpringApplication.run(BrainBashApplication.class, args);
 	}
